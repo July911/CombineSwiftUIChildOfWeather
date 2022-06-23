d042470952d8974e80d6b732b45a209b1cd7ace3
July <min95911@nate.com>
2022-06-23 13:50:47 +0900

    feat: DetailView UI변경

--- ChildOfWeatherCombine/Presentation/DetailWeatherView/View/DetailWeatherView.swift
+++ ChildOfWeatherCombine/Presentation/DetailWeatherView/View/DetailWeatherView.swift
@@ -13,25 +13,35 @@ struct DetailWeatherView: View {
     let city: City
     
     var body: some View {
-        VStack {
-            Text("도시의 이름은?\(city.name)")
-            Text(self.viewModel.weather.sunrise.toKoreanTime.description)
-                .padding()
-            Text(self.viewModel.weather.maxTemperature.toCelsius.description)
-                .font(.title)
-            Text(self.viewModel.weather.minTemperature.toCelsius.description)
-            Text(self.viewModel.weather.sunset.toKoreanTime.description)
+        ZStack {
+            Spacer()
+            
+            Rectangle()
+                .background(Color.gray)
+                .opacity(0.3)
+                .shadow(color: Color.gray, radius: 10, x: 0, y: 10.0)
+                .cornerRadius(10)
+            
+            HStack {
+                Text("오늘의 일출은? \(self.viewModel.weather.sunrise.toKoreanTime.description)")
+                Text("오늘의 일몰은?\(self.viewModel.weather.sunset.toKoreanTime.description)")
+                Text("오늘의 최고온도는?\(self.viewModel.weather.maxTemperature.toCelsius.description)")
+                Text("오늘의 최저온도는?\(self.viewModel.weather.minTemperature.toCelsius.description)")
+            }
+            .font(.callout)
         }
-        .frame(width: 100, height: 500, alignment: .center)
         .onAppear {
             Task {
                 await self.viewModel.fetchWeather(city: city)
             }
-            print("도시의 이름은? \(city.name)")
         }
+    }
 }
-}
-//
+//Text(self.viewModel.weather.sunrise.toKoreanTime.description)
+//Text(self.viewModel.weather.maxTemperature.toCelsius.description)
+//Text(self.viewModel.weather.minTemperature.toCelsius.description)
+//Text(self.viewModel.weather.sunset.toKoreanTime.description)
+////
 //struct DetailWeatherView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        let viewModel = AppDIContainer.shared.detailWeatherDependencies()
